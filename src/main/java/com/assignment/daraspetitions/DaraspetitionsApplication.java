3fcd7961330d935403af03fe66bc9099e9d34ba5
Dara <dara.macmeanmain@gmail.com>
2023-11-14 18:48:13 +0000

    initial commit

---
+++ src/main/java/com/assignment/daraspetitions/DaraspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.daraspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaraspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaraspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/daraspetitions/DaraspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.daraspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaraspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
