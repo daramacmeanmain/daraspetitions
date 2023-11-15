c8173195fe68e2b192c449cc612026c02c47ed32
Dara <dara.macmeanmain@gmail.com>
2023-11-15 16:48:16 +0000

    test spring

--- src/main/java/com/assignment/daraspetitions/DaraspetitionsApplication.java
+++ src/main/java/com/assignment/daraspetitions/DaraspetitionsApplication.java
@@ -2,11 +2,25 @@ package com.assignment.daraspetitions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DaraspetitionsApplication {
 
+	@RequestMapping("/")
+	public String hello1() {
+		return "Hello";
+	}
+
+	@RequestMapping("/hello")
+	public String hello2() {
+		return "Hello Again";
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DaraspetitionsApplication.class, args);
 	}
 
