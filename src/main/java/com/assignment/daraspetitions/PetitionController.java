308c6f3a82397c991a3e1a94482de5785cc75c22
Dara <dara.macmeanmain@gmail.com>
2023-11-24 22:53:09 +0000

    cleaned up web app

--- src/main/java/com/assignment/daraspetitions/PetitionController.java
+++ src/main/java/com/assignment/daraspetitions/PetitionController.java
@@ -9,14 +9,13 @@ import java.util.*;
 @Controller
 public class PetitionController {
 
+    //declare and initialize variables
     List<Petition> petitionList = new ArrayList<>();
-    //List<List<User>> signList = new ArrayList<>();
     HashMap<Integer, ArrayList<User>> userSign = new HashMap<>();
-    ArrayList<User> userList = new ArrayList<>();
-    int userCount = 0;
     int count = 0;
     int numOfSigns = 0;
 
+    //method for searching by title
     public int findByTitle(String title){
         for (Petition p : petitionList){
             if(p.getTitle().equals(title)){
@@ -65,23 +64,11 @@ public class PetitionController {
         model.addAttribute("userSign", userSign);
         model.addAttribute("id", id);
         model.addAttribute("user", user);
-        //model.addAttribute("signList", signList);
         model.addAttribute("petitionList", petitionList);
         model.addAttribute("numOfSigns", numOfSigns);
-        //userList.get(numOfSigns).add(user);
-        //signList.get(id).add(userList.get(numOfSigns));
-        //userList.add(user);
         userSign.computeIfAbsent(id, i -> new ArrayList<>()).add(user);
         petitionList.get(id).setSignatureCount(petitionList.get(id).getSignatureCount() + 1);
-        //System.out.println(signList.get(id));
-        //System.out.println(userList.get(id).getName());
-        for (User u : userSign.get(id)) {
-            System.out.println(id + " - " + u.getName());
-        }
-        //System.out.println(id + " ----- " + userSign.get(id).toString());
-        //System.out.println();
-        //System.out.println(numOfSigns + " " + petition.getTitle() + " " + user.getEmail() + " ---- " + petition.getSignatureCount());
-        return "viewPetition";
+        return "redirect:/viewPetition/?id=" + id;
     }
 
     //retrieve the petition based on id
@@ -91,28 +78,24 @@ public class PetitionController {
         model.addAttribute("id", id);
         model.addAttribute("petitionList", petitionList);
         model.addAttribute("user", new User());
-        //System.out.println("HELLO" + id);
         return "viewPetition";
     }
 
     // return the petition search
     @GetMapping("/searchPetitions")
     public String searchSubmit(Model model) {
-        //model.addAttribute("searchPetition");
         return "searchPetitions";
     }
 
     //retrieve the petition based on searched title
     @RequestMapping("/searchPetitions/")
     public String petitionByTitle(@RequestParam(value="title", required = false) String title, Model model) {
+        model.addAttribute("userSign", userSign);
         model.addAttribute("title", title);
         model.addAttribute("user", new User());
         int id = findByTitle(title);
         model.addAttribute("id", id);
-
         model.addAttribute("petitionList", petitionList);
-        //System.out.println("HELLO" + title);
-        //System.out.println("Index is " + findByTitle(title));
         return "viewPetition";
     }
 
